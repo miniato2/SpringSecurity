4d0cdbf515b909c65ebd3af4d1af32be747cec98
miniato2 <whgg456@gmail.com>
2024-10-10 22:59:02 +0900

    feat : JWTutil

---
+++ src/main/kotlin/org/example/securityoauth/auth/CustomSuccessHandler.kt
@@ -0,0 +1,20 @@
+package org.example.securityoauth.auth
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+
+@Component
+class CustomSuccessHandler : SimpleUrlAuthenticationSuccessHandler() {
+
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authentication: Authentication?
+    ) {
+        super.onAuthenticationSuccess(request, response, authentication)
+    }
+}
