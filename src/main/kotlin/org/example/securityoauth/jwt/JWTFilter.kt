976c29e3cd1b0e063bb112fbd479c3e6d45c6f83
miniato2 <whgg456@gmail.com>
2024-10-13 20:18:10 +0900

    feat : JWTFilter

--- src/main/kotlin/org/example/securityoauth/jwt/JWTFilter.kt
+++ src/main/kotlin/org/example/securityoauth/jwt/JWTFilter.kt
@@ -5,13 +5,34 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.filter.OncePerRequestFilter
 
-class JWTFilter : OncePerRequestFilter() {
+class JWTFilter(
+    private val jwtUtil: JWTUtil
+) : OncePerRequestFilter() {
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
 
-        TODO("Not yet implemented")
+        //쿠키에서 JWT 추출
+        var authorization: String? = null
+        val cookies = request.cookies
+
+        cookies?.let {
+            for (cookie in it) {
+                println(cookie.name)
+                if (cookie.name == "Authorization") {
+                    authorization = cookie.value
+                }
+            }
+        }
+
+        //인증이 필요하지 않은 요청에 대해서는 다음필터로 넘겨줌
+        if(authorization == null){
+            filterChain.doFilter(request, response)
+            return
+        }
+
+
     }
 }
